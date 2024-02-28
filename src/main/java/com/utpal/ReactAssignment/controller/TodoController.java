a3dff35061a4422ae8278b7946945b76c41174b3
Utpal Kant <82315931+Utpalkant6204@users.noreply.github.com>
2024-02-28 19:04:54 +0530

    Update TodoController.java

--- src/main/java/com/utpal/ReactAssignment/controller/TodoController.java
+++ src/main/java/com/utpal/ReactAssignment/controller/TodoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:5173/")
+@CrossOrigin("https://todo-spring-boot-frontend.vercel.app/")
 public class TodoController {
 
 
